7305ca92eb14e6eee55eeff8b2be20471f455af2
EzopovAV <EzopovAV@mail.ru>
2020-02-06 20:07:23 +0400

    Restored after adding .gitignore and deleting project

---
+++ WeatherInCity.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeatherInCity.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeatherInCity.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eaf67e6e-d821-49bd-a913-a2c99869a7e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WeatherInCity/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WeatherInCity
+{
+    public class Item
+    {
+        public string City { get; set; }
+        public int Year { get; set; }
+        public int Temperature { get; set; }
+    }
+}
