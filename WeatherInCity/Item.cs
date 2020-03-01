c0b82b817a38046887734b8d477354ea536dcaee
EzopovAV <EzopovAV@mail.ru>
2020-03-01 11:51:31 +0400

    Add override Equals() and GetHashCode() to Item

--- WeatherInCity/Item.cs
+++ WeatherInCity/Item.cs
@@ -4,7 +4,21 @@ namespace WeatherInCity
 {
     public class Item : IEquatable<Item>
     {
-        public string City { get; set; }
+        private  string _city;
+        private string _cityToLower;
+        
+        public string City
+        {
+            get
+            {
+                return _city;
+            }
+            set
+            {
+                _city = value;
+                _cityToLower = value.ToLower();
+            }
+        }
         public int Year { get; set; }
         public int Temperature { get; set; }
 
@@ -12,7 +26,20 @@ namespace WeatherInCity
         {
             if (Object.ReferenceEquals(this, other)) return true;
             if (Object.ReferenceEquals(other, null)) return false;
-            return this.City == other.City && this.Year == other.Year && this.Temperature == other.Temperature;
+            return this._cityToLower == other._cityToLower && this.Year == other.Year && this.Temperature == other.Temperature;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Item item = obj as Item;
+            return this.Equals(item);
+        }
+
+        public override int GetHashCode()
+        {
+            return this._cityToLower.GetHashCode()
+                ^ this.Year.GetHashCode()
+                ^ this.Temperature.GetHashCode();
         }
     }
 }
